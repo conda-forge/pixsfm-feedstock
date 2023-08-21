28b43cb1be39a2cf0f452c4203a4c7a98f521b7f
Tobias Fischer <info@tobiasfischer.info>
2023-08-22 08:38:38 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,15 @@
-mkdir build
-cd build
+#!/bin/bash
 
-cmake ${CMAKE_ARGS} .. \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DPYTHON_EXECUTABLE=$PYTHON
+# mkdir build
+# cd build
 
-cmake --build . --config Release -- -j1
-cmake --build . --config Release --target install
+# cmake ${CMAKE_ARGS} .. \
+#       -DCMAKE_BUILD_TYPE=Release \
+#       -DCMAKE_PREFIX_PATH=$PREFIX \
+#       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#       -DCMAKE_INSTALL_LIBDIR=lib \
+#       -DPYTHON_EXECUTABLE=$PYTHON
+
+# cmake --build . --config Release -- -j1
+# cmake --build . --config Release --target install
+${PYTHON} -m pip install . --no-deps -vv
